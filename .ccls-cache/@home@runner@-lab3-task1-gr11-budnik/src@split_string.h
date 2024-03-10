e9f8ebb7afad1189d9c9643d919cfad9975abf87
ICraZZyBoY <73648312+ICraZZyBoY@users.noreply.github.com>
2024-03-10 09:22:53 +0000

    split_string
    added 2 files split_string.h and split_string.c which includes split_string function. main.c  uses this func. Client - Interface - Server model.

---
+++ .ccls-cache/@home@runner@-lab3-task1-gr11-budnik/src@split_string.h
@@ -0,0 +1,14 @@
+/**
+Created by Kirill Budnik
+**/
+#ifndef SPLIT_STRING_H
+#define SPLIT_STRING_H
+/**
+void split_string(const char *input, char *evenIndexes, char *oddIndexes) - splits string into two strings, one with even indexes and one with odd indexes
+**/
+#include <stdio.h>
+#include <string.h>
+
+void split_string(const char *input, char *evenIndexes, char *oddIndexes);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
-/* hello.c */
-#include <stdio.h>
+/**
+Created by Kirill Budnik
+Вариант 38
+1. Получить из заданной строки две строки, состоящие из символов первой строки,
+имеющих соответственно четные и нечетные индексы. Не учитывать пробелы в первой
+строке
+**/
+#include "split_string.h"
+#include <stdlib.h>
 
-void main (void)
+int main(int argc, char *argv[]) 
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  if (argc != 2) 
+  {
+    printf("Incorrect arguments\n");
+    return 1;
+  }
+
+  const char *input = argv[1];
+  char *evenIndexes = malloc((strlen(input) + 1) / 2 + 1);
+  char *oddIndexes = malloc((strlen(input) + 1) / 2 + 1);
+  split_string(input, evenIndexes, oddIndexes);
+
+  printf("Even Indices: %s\n", evenIndexes);
+  printf("Odd Indices: %s\n", oddIndexes);
+  return 0;/
 }
